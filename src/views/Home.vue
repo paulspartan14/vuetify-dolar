8a84d245cabe8ef6719f975ce76f8d891ccfcabf
paulspartan14 <e17081416@itmerida.edu.mx>
2021-01-30 03:16:55 -0600

    todo listo hasta antes de axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-layout :wrap="true">
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker
+            v-model="date"  
+            full-width   
+            locale="es-mx"   
+            :min="minimDate"    
+            :max="maxDate"
+          ></v-date-picker>
+        </v-card>
+
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            650 - {{ date }}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      date: '',
+      minimDate: '1984',
+      maxDate: new Date().toISOString().substring(0, 10)
+    }
   }
 }
 </script>
